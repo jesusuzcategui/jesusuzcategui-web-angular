576d1d10ce2162530ec34091a7a02b0d363a14de
Jesus Uzcategui <uzcateguijesusdev@gmail.com>
2024-11-14 21:53:20 -0500

    [add] header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,17 @@ import { RouterModule } from '@angular/router';
 import { routes } from './app.routes';
 import { AppComponent } from './app.component';
 import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainLayoutComponent
+    MainLayoutComponent,
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes) // Asegúrate de importar RouterModule aquí
+    RouterModule.forRoot(routes), // Asegúrate de importar RouterModule aquí,
+    SharedModule
   ],
   bootstrap: [AppComponent]
 })
--- src/app/layouts/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SharedModule } from "../../shared/shared.module";
+import { HeaderComponent } from "../../shared/header/header.component";
+import { FooterComponent } from "../../shared/footer/footer.component";
 
 @Component({
   standalone: true,
   selector: 'app-main-layout',
   templateUrl: './main-layout.component.html',
-  imports: [RouterModule]  // Importa RouterModule aquí si es un standalone component
+  imports: [RouterModule, SharedModule, HeaderComponent, FooterComponent],
 })
 export class MainLayoutComponent {}
